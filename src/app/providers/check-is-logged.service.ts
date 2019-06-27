b142c82c37c0e75679626df665cac5d734516cb5
Luis Eduardo Farfan Melgar <lucho.farfan9@gmail.com>
2019-06-27 04:55:41 -0500

    feat: change check lloged service to router map detail

--- src/app/providers/check-is-logged.service.ts
+++ src/app/providers/check-is-logged.service.ts
@@ -13,7 +13,11 @@ export class CheckIsLoggedService implements CanLoad {
     canLoad() {
         return this.storage.get(KEY_USER_STORAGE).then(res => {
             if (res) {
-                this.router.navigate(['/apps']);
+                if (res.role === 1) {
+                    this.router.navigate(['/map-detail']);
+                } else {
+                    this.router.navigate(['/apps']);
+                }
                 return false;
             } else {
                 return true;
